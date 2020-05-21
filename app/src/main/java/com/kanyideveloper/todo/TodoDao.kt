c4dc07b2cc1b92eb7c46cbee2bbc3090c346c614
JoelKanyi <joelkanyi98@gmail.com>
2020-05-21 14:07:39 +0300

    commented on the data access object interface

--- app/src/main/java/com/kanyideveloper/todo/TodoDao.kt
+++ app/src/main/java/com/kanyideveloper/todo/TodoDao.kt
@@ -2,7 +2,7 @@ package com.kanyideveloper.todo
 
 import androidx.room.*
 import com.kanyideveloper.todo.ToDo
-/*/**
+/**
 This is an interface which acts is an
 intermediary between the user and the database.
 All the operation to be performed on a table
@@ -20,42 +20,13 @@ interface TodoDao{
      * ASC -> Ascending order
      * WHERE -> This is a condition used to query data
      * */
-    @Query("SELECT * FROM todo ORDER BY tId ASC")
-    fun getTodoList(tId:Int) :ToDo
-        /**
-         * @param todo is what we want to save in our database
-         * so many conflict can occur when a data is to be saved, the strategy is used to handle such conflicts
-         * Abort -> this aborts the transaction
-         * Ignore -> this ignores and continues the transaction
-         * Replace -> this replace the data
-         * others includes fail, and roolback
-         * */
-
-        @Insert(onConflict = OnConflictStrategy.IGNORE)
-        fun saveTodo(todo: ToDo)
-        @Update()
-        fun updateTodo(todo:ToDo)
-
-        @Delete
-        fun removeTodo(toDo: ToDo)
-}*/
-
-
-@Dao
-interface TodoDao{
-
-    /**
-     * SELECT -> This retrieve rows from a table in a database
-     * FROM -> You specify the table to retrieve the rows from
-     * ORDER BY -> This is just a sort algorithm
-     * ASC -> Ascending order
-     * WHERE -> This is a condition used to query data
-     * */
     @Query("SELECT*FROM todo ORDER BY tId ASC")
+    //function to list all todo
     fun getTodoList(): List<ToDo>
 
 
     @Query("SELECT*FROM todo WHERE tId=:tid")
+    //function to get a specific todo
     fun getTodoItem(tid: Int): ToDo
     /**
      * @param todo is what we want to save in our database
@@ -66,11 +37,14 @@ interface TodoDao{
      * others includes fail, and roolback
      * */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
+    //add a new todo to the database
     fun saveTodo(todo: ToDo)
 
     @Update
+    //making changes to an existing todo
     fun updateTodo(todo: ToDo)
 
     @Delete
+    //deletes a todo from the database
     fun removeTodo(todo: ToDo)
 }
