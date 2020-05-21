c71333de4540a3c02e10cf79c37723fc80fd695b
JoelKanyi <joelkanyi98@gmail.com>
2020-05-21 13:59:20 +0300

    more comments on the Todo class

--- app/src/main/java/com/kanyideveloper/todo/ToDo.kt
+++ app/src/main/java/com/kanyideveloper/todo/ToDo.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-/**This is nothing but a model class annotated
+/**This is a model class annotated
 with @Entity where all the variable will becomes
 column name for the table and name of the
 model class becomes name of the table.
@@ -14,8 +14,11 @@ variables are the columns of the table.*/
 //So the table will have three columns- todo_title,todo_priority and the table ID
 
 @Entity(tableName = "todo")
+
+//the class name is the table name
 class ToDo(
     @ColumnInfo(name = "todo_title")
+    //the variable are the column names
     var title:String = "",
     @ColumnInfo(name = "todo_priority")
     var priority: Int = 0,
